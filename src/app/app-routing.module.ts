d3ee32bbc0a72e66e8c22fed1b5e44a2aa4288c0
Giorgi Mindiashvili <126167738+MindiashviliGiorgi@users.noreply.github.com>
2023-09-21 23:53:58 +0400

    auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { AdminPanelPageComponent } from './admin-panel-page/admin-panel-page.component';
+import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
 
 const routes: Routes = [
-  {
-    path : '',
-    component : AdminPanelPageComponent
-  }
+  {path : '', redirectTo : "login", pathMatch : 'full'},
+  {path : 'login', component : LoginComponent},
+  {path : 'dashboard', component : DashboardComponent},
+  {path : 'register', component : RegisterComponent},
+
 ];
 
 @NgModule({
