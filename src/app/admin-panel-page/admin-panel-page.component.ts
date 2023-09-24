4493ef116d46aa7e4e40fd8b6799e66d7dfcbeb6
Giorgi Mindiashvili <126167738+MindiashviliGiorgi@users.noreply.github.com>
2023-09-24 21:23:57 +0400

    get

--- src/app/admin-panel-page/admin-panel-page.component.ts
+++ src/app/admin-panel-page/admin-panel-page.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
 import { ProductForm } from '../auth/auth';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-admin-panel-page',
@@ -10,7 +11,9 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class AdminPanelPageComponent {
 
-  formWindow : boolean = true;
+  allProducts : ProductForm[] = [];
+
+  formWindow : boolean = false;
 
   productList : ProductForm[] = [];
 
@@ -25,7 +28,11 @@ export class AdminPanelPageComponent {
   constructor(private authService : AuthService, private http : HttpClient){}
 
   ngOnInit():void {
-    
+    this.fetchProduct();
+  }
+
+  onProductsFetch(){
+    this.fetchProduct();
   }
 
   userLogout(){
@@ -34,10 +41,28 @@ export class AdminPanelPageComponent {
 
   onProductCreate(products : ProductForm){
     const headers = new HttpHeaders({'myHeader' : 'GM'});
-    this.http.post('https://gm-97a5f-default-rtdb.europe-west1.firebasedatabase.app/products.json', products,
+    this.http.post<{name : string}>(
+      'https://gm-97a5f-default-rtdb.europe-west1.firebasedatabase.app/products.json', products,
     {headers : headers })
     .subscribe((res) => {
       console.log(res)
+    })
+  }
+
+  private fetchProduct(){
+    this.http.get('https://gm-97a5f-default-rtdb.europe-west1.firebasedatabase.app/products.json')
+    .pipe(map((res) => {
+      const products = [];
+      for(const key in res){
+        if(res.hasOwnProperty(key)){
+          products.push({...res[key], id: key})
+        }
+      }
+      return products;
+    }))
+    .subscribe((products) => {
+      console.log(products)
+      this.allProducts = products;
     })
   }
 
